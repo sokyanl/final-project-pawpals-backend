b5e076a1d20143c3a901662eff70d5fb607553b6
elenayap <elenayap.27@gmail.com>
2024-03-14 15:39:43 +0800

    added pet controller

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-import express from "express"
-import prisma from "./src/utils/prisma.js"
-import { Prisma } from "@prisma/client"
+import express from "express";
+import prisma from "./src/utils/prisma.js";
+import { Prisma } from "@prisma/client";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./src/controllers/users.controllers.js";
+import authRouter from "./src/controllers/auth.controllers.js";
+import auth from "./src/middlewares/auth.js";
+import petRouter from "./src/controllers/pet.controllers.js";
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+app.use(morgan('combined'))
 
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
+app.use("/pet", petRouter);
 
-
-// app.use("/users", userRouter);
-// app.use("/auth", authRouter);
-// app.use("/pet", petRouter);
-
+app.get('/protected', auth, (req, res) => {
+    res.json({ "hello": "world" })
+  });
 
 
 export default app;
-
